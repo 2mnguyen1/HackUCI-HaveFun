5cba029860caa46c79bb53e6a957c5cf1942f66b
NwinNwin <ndnguyen2003@gmail.com>
2023-02-03 21:56:32 -0800

    login page

--- app-ui/src/App.jsx
+++ app-ui/src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./LoginPage/Login";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ app-ui/src/LoginPage/Login.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <>
+      <div className="login-main">
+        <div className="login-input">
+          <div className="login-logo">
+            <img
+              src="https://media.discordapp.net/attachments/795161297121574932/1071291088414986280/H-removebg-preview.png"
+              alt=""
+            />
+            <h1>HelpFun</h1>
+          </div>
+
+          <form action="">
+            <h1>Login/Register</h1>
+
+            <input className="login-box" type="text" placeholder="Username" />
+            <input
+              className="login-box"
+              type="password"
+              placeholder="Password"
+            />
+            <button className="login-btn" type="submit">
+              + LOGIN
+            </button>
+          </form>
+        </div>
+        <div className="login-pic">
+          <img
+            src="https://media.discordapp.net/attachments/795161297121574932/1071306318620663868/DALL_E_2023-02-03_20.21_1.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+}
