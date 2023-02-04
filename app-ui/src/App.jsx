205623ba2af6247cd383d703db288189d0e240ef
2mnguyen1 <2mnguyen1@gmail.com>
2023-02-04 01:21:06 -0800

    finish middle pate

--- app-ui/src/App.jsx
+++ app-ui/src/App.jsx
@@ -3,14 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./LoginPage/Login";
 import Navbar from "./Navbar/Navbar"
 import LeftMainPage from "./LeftMainPage/LeftMainPage";
-
+import RightMainPage from "./RightMainPage/RightMainPage";
+import "./App.css"
 
 export default function App() {
   return (
     <div>
       <div>
           <Navbar />
-          <LeftMainPage />
+
+          <div className="home-page">
+            <LeftMainPage />
+            <RightMainPage />
+          </div>
+
       </div>
     
       <BrowserRouter>
