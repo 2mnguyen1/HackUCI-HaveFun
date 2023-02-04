e6857847f5f9b8489ac641d02145d90b22e7b068
NwinNwin <ndnguyen2003@gmail.com>
2023-02-04 13:45:35 -0800

    added timer and edited main ui

--- app-ui/src/RightMainPage/RightMainPage.jsx
+++ app-ui/src/RightMainPage/RightMainPage.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import TaskToDo from '../components/taskToDo/TaskToDo'
-import "./RightMainPage.css"
-
+import React from "react";
+import TaskToDo from "../components/taskToDo/TaskToDo";
+import "./RightMainPage.css";
 
 export default function RightMainPage() {
   return (
-    <div className='right-main-page-container'>
-        <div className='right-main-page-title'>Tasks to do</div>
-        <TaskToDo />
-        <TaskToDo />
-        <TaskToDo />
+    <div className="right-main-page-container">
+      <div className="right-main-page-title">Happiness To Do</div>
+      <TaskToDo />
+      <TaskToDo />
+      <TaskToDo />
     </div>
-  )
+  );
 }
